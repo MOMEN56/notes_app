c73518a766e7ef915da8cd006d4530af035c691a
MOMEN56 <5momenalaa5@gmail.com>
2024-11-02 19:35:13 +0200

    show model bottom

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom.dart';
 import 'package:notes_app/widgets/notes_body.dart';
 
 class Homescreen extends StatelessWidget {
@@ -6,11 +7,18 @@ class Homescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-    floatingActionButton: FloatingActionButton(onPressed: (){},
-    child: const Icon(Icons.add),
-    ),  
-    body: const NotesBody(),  
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const addNoteBottomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: const NotesBody(),
     );
   }
 }
