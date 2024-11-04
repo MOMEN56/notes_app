520e1c6cb3eb39ecc4ba216e8522392a06dbc608
MOMEN56 <5momenalaa5@gmail.com>
2024-11-04 16:44:31 +0200

    Create Notes Cubit

---
+++ lib/cubits/notes_cubit/notes_cubit_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_cubit_state.dart';
+
+class NotesCubitCubit extends Cubit<NotesState> {
+  NotesCubitCubit() : super(NotesInitial());
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
+}
--- lib/widgets/add_note_bottom.dart
+++ lib/widgets/add_note_bottom.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
-import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
-import 'package:notes_app/widgets/custom_bottom.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({super.key});
@@ -31,9 +27,18 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
           }
         },
         builder: (context, state) {
-          return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoading ? true : false,
-              child: SingleChildScrollView(child: AddNoteForm()));
+          return AbsorbPointer(
+            absorbing: state is AddNoteLoading ? true : false,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  right: 16,
+                  left: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: SingleChildScrollView(
+              child: AddNoteForm(),
+            ),  
+            ),
+          );
         },
       ),
     );
